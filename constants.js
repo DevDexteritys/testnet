e02b12ffb7c90183f0719fe4a4b73b1f1a784b84
Web_sol <36423588+roman-yaremenko@users.noreply.github.com>
2022-09-02 02:39:08 +0300

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,127 @@
+export const contractAddress = "0xC5B4f6495C655D9D251eC1Ac9113E1F407578B56"
+//export const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
+
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "priceFeed",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "MINIMUM_USD",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "cheaperWithdraw",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "fund",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "fundingAddress",
+        type: "address",
+      },
+    ],
+    name: "getAddressToAmountFunded",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "index",
+        type: "uint256",
+      },
+    ],
+    name: "getFunder",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getPriceFeed",
+    outputs: [
+      {
+        internalType: "contract AggregatorV3Interface",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getVersion",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+]
---
+++ index.js
@@ -0,0 +1,98 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress} from "./constants.js"
+import { busdABI, busdAddress} from "./constantsBUSD.js"
+//import { abiBUSD, contractAddressBUSD } from "./constantbusd.js"
+
+const connectButton = document.getElementById("connectButton")
+
+const fundButton = document.getElementById("fundButton")
+
+var viewAmount = document.getElementById("viewAmount")
+
+connectButton.onclick = connect
+
+fundButton.onclick = fund
+
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" })
+    } catch (error) {
+      console.log(error)
+    }
+    connectButton.innerHTML = "Connected"
+    const accounts = await ethereum.request({ method: "eth_accounts" })
+    console.log(accounts)
+  } else {
+    connectButton.innerHTML = "Please install MetaMask"
+  }
+}
+
+
+async function fund() {
+  const ethAmount = document.getElementById("ethAmount").value
+  //const viewAmount = document.getElementById("viewAmount")
+  //viewAmount.innerText = ethAmount
+  console.log(`Funding with ${ethAmount}...`)
+  
+ 
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    try {
+	  
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+		
+      })
+	  document.getElementById("viewAmount").innerText += ethAmount
+      await listenForTransactionMine(transactionResponse, provider)
+	  
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    fundButton.innerHTML = "Please install MetaMask"
+  }
+}
+
+/*async function transferBUSD() {
+	  //const ethAmount = document.getElementById("ethAmount").value
+      console.log(`Funding with ${ethAmount}...`)
+	  
+      if (typeof window.ethereum !== "undefined") {
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		const signer = provider.getSigner()
+		//const busdContract = new ethers.Contract(busdAddress, busdABI, signer);
+        try {
+		  busdContract = new ethers.Contract(busdAddress, busdABI, signer);
+          const ethAmount = document.getElementById("ethAmount").value
+		  busdContract.approve(contractAddress, ethAmount);
+		  contract = new ethers.Contract(contractAddress, abi, signer);
+          //const transactionResponse = await contract.fund({
+          //value: ethers.utils.parseEther(ethAmount),
+
+          
+        } catch (error) {
+          console.error(error.message, error.data.message);
+        } 
+      }
+      else {
+        document.getElementById("transferButton").innerHTML = "Please install MetaMask";
+      }
+    };*/
+
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`)
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionReceipt) => {
+      console.log(
+        `Completed with ${transactionReceipt.confirmations} confirmations. `
+      )
+      resolve()
+    })
+  })
+}
