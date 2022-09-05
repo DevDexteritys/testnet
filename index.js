4f697c16d29d74701d0128d3ee78e0d3bd96508a
Web_sol <36423588+Rmenchisc@users.noreply.github.com>
2022-09-06 02:58:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ import { ethers } from "./ethers-5.6.esm.min.js"
 import { abi, contractAddress} from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
-
+const dicsonnectButton = document.getElementById("dicsonnectButton")
 const fundButton = document.getElementById("fundButton")
-
 var viewAmount = document.getElementById("viewAmount")
 
 connectButton.onclick = connect
-
+dicsonnectButton.onclick = disconnect
 fundButton.onclick = fund
 
 
@@ -26,6 +25,29 @@ connectButton.innerHTML = "Connected"
   } else {
     connectButton.innerHTML = "Please install MetaMask"
   }
+}
+
+async function disconnect() {
+
+  console.log("Killing the wallet connection", provider);
+
+  // TODO: Which providers have close method?
+  if(provider.close) {
+    await provider.close();
+
+    // If the cached provider is not cleared,
+    // WalletConnect will default to the existing session
+    // and does not allow to re-scan the QR code with a new wallet.
+    // Depending on your use case you may want or want not his behavir.
+    await web3Modal.clearCachedProvider();
+    provider = null;
+  }
+
+  selectedAccount = null;
+
+  // Set the UI back to the initial state
+  document.querySelector("#prepare").style.display = "block";
+  document.querySelector("#connected").style.display = "none";
 }
 
 
